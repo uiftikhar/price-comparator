d225a73856ecd1b6b99c40dac6f45c8a6a4224f5
umer <umer229@gmail.com>
2019-11-07 21:35:29 +0100

    code refactor

--- src/tariff-calculator/services/tariff-calculator.service.ts
+++ src/tariff-calculator/services/tariff-calculator.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+
 import { NUMBER_OF_MONTHS } from '../../shared/constants/number-of-months';
 import { TariffCalculationResult } from '../interfaces';
 
@@ -25,25 +26,38 @@ export class TariffCalculatorService {
     consumptionAmount: number,
   ): TariffCalculationResult {
     const CONSUMPTION_THRESHOLD = 4000;
-    const isInMinimumThreshold = consumptionAmount < CONSUMPTION_THRESHOLD;
     const CONSUMPTION_COST_PER_KWH = 0.3;
     const BASE_COST = 800;
+    const isInMinimumThreshold = consumptionAmount < CONSUMPTION_THRESHOLD;
+    const basePackage = {
+      baseCosts: BASE_COST,
+      consumptionCosts: 0,
+      annualCost: BASE_COST,
+    };
 
-    if (isInMinimumThreshold) {
-      return {
-        baseCosts: BASE_COST,
-        consumptionCosts: 0,
-        annualCost: BASE_COST,
-      };
-    } else {
-      const differenceOfConsumption = consumptionAmount - CONSUMPTION_THRESHOLD;
-      const calculationResult =
-        BASE_COST + differenceOfConsumption * CONSUMPTION_COST_PER_KWH;
-      return {
-        baseCosts: BASE_COST,
-        consumptionCosts: differenceOfConsumption * CONSUMPTION_COST_PER_KWH,
-        annualCost: calculationResult,
-      };
-    }
+    return isInMinimumThreshold
+      ? basePackage
+      : this.computePackagedTariff(
+          consumptionAmount,
+          CONSUMPTION_THRESHOLD,
+          BASE_COST,
+          CONSUMPTION_COST_PER_KWH,
+        );
+  }
+
+  private computePackagedTariff(
+    consumptionAmount: number,
+    consumptionThreshold: number,
+    baseCost: number,
+    consumptionCostPerKwh: number,
+  ): TariffCalculationResult {
+    const differenceOfConsumption = consumptionAmount - consumptionThreshold;
+    const calculationResult =
+      baseCost + differenceOfConsumption * consumptionCostPerKwh;
+    return {
+      baseCosts: baseCost,
+      consumptionCosts: differenceOfConsumption * consumptionCostPerKwh,
+      annualCost: calculationResult,
+    };
   }
 }
