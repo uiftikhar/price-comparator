fba3f1283b31928aed4e2d7c85106568d8817800
umer <umer.iftikhar@akelius.de>
2019-11-07 14:17:31 +0100

    update tests

--- src/tariff-calculator/services/tariff-calculator.service.spec.ts
+++ src/tariff-calculator/services/tariff-calculator.service.spec.ts
@@ -1,8 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TariffCalculatorService } from './tariff-calculator.service';
+import { TariffCalculationResult } from '../interfaces';
 
 describe('TariffCalculatorService', () => {
   let service: TariffCalculatorService;
+  const basicResult: TariffCalculationResult = {
+    baseCosts: 60,
+    consumptionCosts: 1320,
+    annualCost: 1380,
+  };
+
+  const packagedResult: TariffCalculationResult = {
+    baseCosts: 800,
+    consumptionCosts: 600,
+    annualCost: 1400,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,7 +25,26 @@ describe('TariffCalculatorService', () => {
   });
 
   it('should be defined', () => {
-    // expect(service).toBeDefined();
-    expect(1).toEqual(1);
+    expect(service).toBeDefined();
+  });
+
+  describe('basicElectricityTariffCalculation', () => {
+    it('should return base electricity cost', () => {
+      const consumptionAmount = 6000;
+      const mockServiceCall = service.basicElectricityTariffCalculation(
+        consumptionAmount,
+      );
+      expect(mockServiceCall).toEqual(basicResult);
+    });
+  });
+
+  describe('packagedElectricityTariffCalculation', () => {
+    it('should return packaged electricity cost', () => {
+      const consumptionAmount = 6000;
+      const mockServiceCall = service.packagedElectricityTariffCalculation(
+        consumptionAmount,
+      );
+      expect(mockServiceCall).toEqual(packagedResult);
+    });
   });
 });
