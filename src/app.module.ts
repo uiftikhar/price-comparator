0f1725510e4b3dee35f06b64491622032bafa2ae
Umer Iftikhar <umer229@gmail.com>
2019-11-07 14:40:33 +0100

    Interceptor (#3)
    * add error interceptor and logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PriceComparisonModule } from './price-comparison/price-comparison.module';
 import { TariffCalculatorModule } from './tariff-calculator/tariff-calculator.module';
+import { HttpErrorFilter } from './shared/filter/http-error.filter';
+import { LoggingInterceptor } from './shared/interceptor/logging.interceptor';
 
 @Module({
   imports: [PriceComparisonModule, TariffCalculatorModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/shared/interceptor/logging.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor<T> implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<T> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const { method, url } = request;
+    const now = Date.now();
+
+    return next
+      .handle()
+      .pipe(
+        tap(() =>
+          Logger.log(
+            `${method} ${url} ${Date.now() - now}ms`,
+            context.getClass().name,
+          ),
+        ),
+      );
+  }
+}
